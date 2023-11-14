ce3348f844008cf68c43c4326e36afce133ecc01
101 <tmddnjs001133@dsm.hs.kr>
2023-11-14 20:01:50 +0900

    db 연결 로그 작성 및 예외 처리

--- src/api/test.js
+++ src/api/test.js
@@ -4,12 +4,13 @@ const app = express()
 app.listen(3000, () => {
     console.log('실행 중 포트 : 3000')
 })
+app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('')
-// const db = mongoose.connection
 
-app.use(express.json())
+mongoose.connect('')
+        .then(() => console.log('db 연결 성공'))
+        .catch((error) => console.log(error))
 
 const userSchema = new mongoose.Schema({
     name: String,
