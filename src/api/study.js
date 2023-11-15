ce2d24d096ab161c381d13dc8033d9c7fd6f118f
101 <tmddnjs001133@dsm.hs.kr>
2023-11-15 14:52:26 +0900

    게시물 저장 로직 추가

--- src/api/study.js
+++ src/api/study.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import Board from '/model/board.js'
 
 const router = express.Router()
 
 router.post('/board', async(req, res) => {
     const {title, content} = req.body
+
+    const board = Board({title, content})
+    await board.save()
 })
