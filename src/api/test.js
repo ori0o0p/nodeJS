fc52396ac16ccf2f4cc5039ec8bb154f12f08b19
101 <tmddnjs001133@dsm.hs.kr>
2023-11-15 11:05:02 +0900

    스키마 생성

--- src/api/test.js
+++ src/api/test.js
@@ -12,7 +12,20 @@ import mongoose from 'mongoose'
 mongoose.connect('mongodb://localhost:27017/nodejs', {useNewUrlParser: true})
         .then(() => console.log('db 연결!'))
         .catch((error) => console.error(error))
+        
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
 
+const user = new mongoose.model('user', userSchema)
+
+app.post('/', async (req, res) => {
+    const user = new user({
+        name: 'test',
+        age: 111
+    })
+}) 
 
 app.get('/hello', (req, res) => {
     res.json({'message': 'lol',
