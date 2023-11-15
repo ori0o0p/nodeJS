ecb4f3f3b656aabc98ef0943accaf0b23248d7f9
101 <tmddnjs001133@dsm.hs.kr>
2023-11-15 11:04:31 +0900

    mongodb 연결

--- src/api/test.js
+++ src/api/test.js
@@ -7,6 +7,13 @@ app.listen(3000, () => {
 })
 app.use(express.json())
 
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb://localhost:27017/nodejs', {useNewUrlParser: true})
+        .then(() => console.log('db 연결!'))
+        .catch((error) => console.error(error))
+
+
 app.get('/hello', (req, res) => {
     res.json({'message': 'lol',
               'point': '/hello'})
